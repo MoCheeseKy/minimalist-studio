28bd44e476ce563ed1d8c5a00729b8ab5a412a75
Kontra_N356 <naufalnabillansyah49@gmail.com>
2024-11-16 15:57:46 +0700

    init: database model initialization

---
+++ prisma/migrations/20241116085425_table_intialization/migration.sql
@@ -0,0 +1,97 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('Pria', 'Perempuan');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "fullname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "instagram" TEXT NOT NULL,
+    "birth_date" TIMESTAMP(3) NOT NULL,
+    "quota" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Studio" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "capacity" INTEGER NOT NULL,
+    "location" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Studio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Class_Category" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Class_Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Class" (
+    "id" TEXT NOT NULL,
+    "category_id" INTEGER NOT NULL,
+    "studio_id" TEXT NOT NULL,
+    "start_at" TIMESTAMP(3) NOT NULL,
+    "end_at" TIMESTAMP(3) NOT NULL,
+    "description" TEXT NOT NULL,
+    "instructor" TEXT NOT NULL,
+    "current_slot" INTEGER NOT NULL,
+    "max_slot" INTEGER NOT NULL,
+    "req_quota" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Class_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User_Class" (
+    "class_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_Class_pkey" PRIMARY KEY ("class_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Studio_id_key" ON "Studio"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Class_Category_id_key" ON "Class_Category"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Class_id_key" ON "Class"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_Class_class_id_key" ON "User_Class"("class_id");
+
+-- AddForeignKey
+ALTER TABLE "Class" ADD CONSTRAINT "Class_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Class_Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Class" ADD CONSTRAINT "Class_studio_id_fkey" FOREIGN KEY ("studio_id") REFERENCES "Studio"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User_Class" ADD CONSTRAINT "User_Class_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "Class"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User_Class" ADD CONSTRAINT "User_Class_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
