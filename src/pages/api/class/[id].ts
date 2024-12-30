ac5850f3ed57473a12c73f1f9ab85e131b5a4c30
Kontra_N356 <naufalnabillansyah49@gmail.com>
2024-12-30 15:23:44 +0700

    feat: Update and Delete for class (has an effect to user_class and user Quota || db: update relation in class to user class)

--- src/pages/api/class/[id].ts
+++ src/pages/api/class/[id].ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client';
 import { respond } from '@/utils/resJson';
-import { categoryValidSchema } from './validation.schema';
+import { classUpdateValidSchema } from './validation.schema';
 import { error } from 'console';
 const prisma = new PrismaClient();
 
@@ -61,5 +61,117 @@ async function getByIdClass(
     } catch (error) {
         console.log(error)
         return respond(500, true, "Internal Server Error", null, res);        
+    }
+}
+
+async function updateClass(
+    req:NextApiRequest,
+    res:NextApiResponse
+) {
+    try {
+        const {id} = req.query
+        if(!id){
+            return respond(400, true, "Id harus disertakan", null, res)
+        }
+        
+        const unUpdatedClass = await prisma.class.findUnique({
+             where:{id:String(id)}
+        })
+        if(!unUpdatedClass){
+            return respond(404, true, `Kategori id-${id} tidak ada`, null, res);
+        }
+        
+        const validationResult = classUpdateValidSchema.validate(req.body)
+        if(validationResult.error){
+            const errorMessage = validationResult.error.details.map(err => err.message);
+            return respond(400, true, errorMessage, null, res);
+        }
+        
+        const updatedClass = await prisma.class.update({
+            where:{id:String(id)},
+            data:validationResult.value
+        })
+        
+        if(updatedClass.req_quota == unUpdatedClass.req_quota){
+            return respond(200, false, `Kelas id-${id} Berhasil Diperbarui`, updatedClass, res)
+        } 
+        
+        await returnQuotaUser(String(id), updatedClass.req_quota, unUpdatedClass.req_quota)
+
+        return respond(200, false, `Kelas id-${id} Berhasil Diperbarui, dan karena kuota berubah maka beberapa kuota dikembalikan`, updatedClass, res)
+    } catch (error) {
+        console.log(error)
+        return respond(500, true, "Internal Server Error", null, res);        
+    }
+}
+
+
+async function deleteClass(
+    req:NextApiRequest,
+    res:NextApiResponse
+) {
+    try {   
+        const {id} = req.query
+            if(!id){
+            return respond(400, true, "Id harus disertakan", null, res)
+        }
+
+        const YogaClass = await prisma.class.findUnique({
+            where:{id:String(id)}
+        })
+        if(!YogaClass){
+            return respond(404, true, `Kelas id-${id} tidak ada`, null, res);
+        }
+        
+        await prisma.user_Class.deleteMany({
+            where:{class_id:String(id)}
+        })
+        
+        await prisma.class.delete({
+            where:{id:String(id)}
+        })
+        
+        await prisma.user.updateMany({
+            where:{user_class:{
+                some:{class_id:String(id)}
+            }},
+            data:{quota:{
+                increment:YogaClass.req_quota
+            }}
+        })
+        
+        return respond(200, false, `Kelas id-${id} Berhasil Dihapus`, null, res)
+    } catch (error) {
+        console.log(error)
+        return respond(500, true, "Internal Server Error", null, res);        
+    }
+}
+
+
+async function returnQuotaUser(class_id:string, updatedQuota:number, unUpdateQuota:number){
+    try {
+        let quotaForUser =
+        unUpdateQuota > updatedQuota ?
+        unUpdateQuota - updatedQuota :
+        unUpdateQuota;
+
+        let isEnough = unUpdateQuota > updatedQuota;
+
+        await prisma.user.updateMany({
+            where:{user_class:{
+                some:{class_id: class_id}
+            }},
+            data:{quota:quotaForUser}
+        })
+
+        if(!isEnough){
+            await prisma.user_Class.delete({
+                where:{
+                    class_id: String(class_id)
+                }
+            })
+        }
+    } catch (error) {
+        throw error
     }
 }
